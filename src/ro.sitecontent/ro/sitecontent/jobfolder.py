40c3a61bf9f201e1bb186705b201b41b4cff559c
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-21 18:37:40 +0100

    Add content listing to folder query

--- src/ro.sitecontent/ro/sitecontent/jobfolder.py
+++ src/ro.sitecontent/ro/sitecontent/jobfolder.py
@@ -1,4 +1,6 @@
+from Aquisition import aq_inner
 from five import grok
+from plone import api
 
 from z3c.form import group, field
 from zope import schema
@@ -16,6 +18,7 @@ from plone.namedfile.interfaces import IImageScaleTraversable
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
+from plone.app.contentlisting.interfaces import IContentListing
 
 from ro.sitecontent import MessageFactory as _
 
@@ -34,3 +37,15 @@ class View(grok.View):
     grok.context(IJobFolder)
     grok.require('zope2.View')
     grok.name('view')
+
+    def update(self):
+        self.has_folders = len(self.job_folders()) > 0
+
+    def job_folders(self):
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+        results = catalog(object_provides=IJobFolder.__identifier__,
+                          review_state='published',
+                          sort_on='getObjPositionInParent')
+        items = IContentListing(results)
+        return items
