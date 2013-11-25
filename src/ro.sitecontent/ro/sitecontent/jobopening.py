66e5f5788a7e854deaed266e9774aec1612f3e0f
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-25 15:09:26 +0100

    Celanup job opening schema declarations

--- src/ro.sitecontent/ro/sitecontent/jobopening.py
+++ src/ro.sitecontent/ro/sitecontent/jobopening.py
@@ -1,20 +1,9 @@
 from five import grok
 
-from z3c.form import group, field
-from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
 from plone.dexterity.content import Container
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.directives import form
+from plone.namedfile.field import NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 
 from ro.sitecontent import MessageFactory as _
@@ -24,6 +13,11 @@ class IJobOpening(form.Schema, IImageScaleTraversable):
     """
     A single job opening
     """
+    attachment = NamedBlobFile(
+        title=_(u"Attachement"),
+        description=_(u"Please upload additional attachment preferable as pdf"),
+        required=False,
+    )
 
 
 class JobOpening(Container):
